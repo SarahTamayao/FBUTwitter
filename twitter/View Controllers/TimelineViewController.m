4af6f92d65b498b399353412f9dc103d0acc383e
Ava Crnkovic-Rubsamen <avacrnkovic@fb.com>
2021-06-28 15:42:14 -0400

    user log out

--- twitter/View Controllers/TimelineViewController.m
+++ twitter/View Controllers/TimelineViewController.m
@@ -8,8 +8,13 @@
 
 #import "TimelineViewController.h"
 #import "APIManager.h"
+#import "AppDelegate.h"
+#import "LoginViewController.h"
 
 @interface TimelineViewController ()
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *logoutButton;
+
+
 
 @end
 
@@ -30,6 +35,16 @@
             NSLog(@"😫😫😫 Error getting home timeline: %@", error.localizedDescription);
         }
     }];
+}
+- (IBAction)logoutButtonPressed:(id)sender {
+    NSLog(@"%s", "logout");
+    AppDelegate *appDelegate = (AppDelegate *)[UIApplication sharedApplication].delegate;
+
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    LoginViewController *loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
+    appDelegate.window.rootViewController = loginViewController;
+    
+    [[APIManager shared] logout];
 }
 
 - (void)didReceiveMemoryWarning {
